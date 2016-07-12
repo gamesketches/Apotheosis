271a90f6d33e7552dae64f23076bc48321d671df
gamesketches <sam.von.ehren@gmail.com>
2016-07-11 22:44:02 -0400

    moved bullet spawn point to playe rorigin

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -206,7 +206,7 @@ public class InputManager : MonoBehaviour {
 
 	public void CreateBullet(BulletDepot.Bullet bullet, BulletType type = BulletType.Gator) {
 		int angle = bullet.angle + (int)playerMovement.CurrentShotAngle();
-		BulletLogic bulletLogic = ((GameObject)Instantiate(bulletPrefab, reticle.transform.position, Quaternion.Euler(0, 0, angle))).GetComponent<BulletLogic>();
+		BulletLogic bulletLogic = ((GameObject)Instantiate(bulletPrefab, gameObject.transform.position, Quaternion.Euler(0, 0, angle))).GetComponent<BulletLogic>();
 		bulletLogic.Initialize(type, bullet.damage, bullet.speed, bullet.size, 5, playerStats.playerColor, playerStats.number, playerStats.character);
 	}
 
